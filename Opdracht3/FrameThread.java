a1498f252d1bf717c9976ba0709e6c05a24bab12
Tommie Terhoeve <tommieterhoeve@gmail.com>
2017-02-14 20:18:09 +0100

    IT WORKS!

--- Opdracht3/FrameThread.java
+++ Opdracht3/FrameThread.java
@@ -1,13 +1,64 @@
 
+import javax.swing.*;
+import java.awt.event.*;
+import java.text.ParseException;
+import java.awt.*;
 
 public class FrameThread extends Thread{
 
+    private JLabel time;
+    private JButton start;
+    private JButton pause;
+    private JButton reset;
+    private JFrame f;
+    private String solution;
+    public boolean counter;
+    TimerThread stopwatch;
+    MyMouse x;
+
+    public FrameThread(){
+        f = new JFrame("Assignment 3");
+        x = new MyMouse();
+        stopwatch = new TimerThread();
+        f.setLayout(new java.awt.GridLayout(4,3));
+        f.setSize(600,400);
+        f.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        //initialiseer componenten
+        time  = new JLabel();
+        start = new JButton("START");
+        pause = new JButton("PAUSE");
+        reset = new JButton("RESET");
+        start.addMouseListener(x);
+        pause.addMouseListener(x);
+        reset.addMouseListener(x);
+        counter = false;
+
+        f.add(time);
+        f.add(start);
+        f.add(reset);
+
+        f.setVisible(true);
+        }
+
     public void run(){
+        while(stopwatch.difference < 10000)
+        time.setText(Long.toString(stopwatch.difference));
+    }
 
-        CustomFrame i = new CustomFrame();
-        while(i.counter == false){
+    private class MyMouse extends MouseAdapter{
 
-        i.update();
+        public void mouseClicked(MouseEvent e ){
+
+            if(e.getSource() == start){
+
+                stopwatch.start();
+
+            }
+
+            if(e.getSource() == reset){
+                stopwatch.difference = 0;
+            }
         }
     }
 }
