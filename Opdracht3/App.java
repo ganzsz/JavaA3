8d2b811bbd589ad098792a99184ecc52df159e79
Tommie Terhoeve <tommieterhoeve@gmail.com>
2017-02-16 11:09:24 +0100

    gggg

--- Opdracht3/App.java
+++ Opdracht3/App.java
@@ -3,38 +3,45 @@ import java.awt.event.*;
 
 public class App{
 
-    private static JButton startStop;
-    private static JButton reset;
-    private static boolean alive;
+    private static int lapCounter;
+    private static String [] laps;
+    private static boolean reset;
     private static boolean pause;
     private static boolean startOrStop;
-    private static JLabel display;
-    private static Thread countdown;
+    private static JButton startStop;
+    private static JButton resetButton;
+    private static JPanel buttonPanel;
+    private static JPanel timerPanel;
 
     public static void main(String [] args){
+        DefaultListModel lapModel = new DefaultListModel();
+        JList lapTime = new JList(lapModel);
+        lapCounter = 1;
+        laps = new String[6];
+        for(int i = 0; i < 6; i++){
+            lapModel.addElement("00:00:00");
+        }
         startOrStop = true;
-        alive = true;
         pause = true;
-
+        reset = false;
         //interface
         JFrame frame = new JFrame("assignment 3");
+        buttonPanel = new JPanel();
+        timerPanel = new JPanel();
+        startStop = new JButton("START/PAUSE");
+        resetButton = new JButton("RESET");
         frame.setSize(300,200);
         frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(new java.awt.FlowLayout());
-        startStop = new JButton("START/PAUSE");
-        reset = new JButton("RESET");
-        display = new JLabel("00:00:00");
-        JPanel buttonPanel = new JPanel();
-        JPanel timerPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
         timerPanel.setLayout(new BoxLayout(timerPanel, BoxLayout.PAGE_AXIS));
         buttonPanel.add(startStop);
-        buttonPanel.add(reset);
-        timerPanel.add(display);
+        buttonPanel.add(resetButton);
+        timerPanel.add(lapTime);
         frame.add(buttonPanel);
         frame.add(timerPanel);
         frame.setVisible(true);
-
+        //buttonlisteners
         startStop.addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent evt)
@@ -42,14 +49,15 @@ public class App{
                 buttonPressed(evt);
             }
         });
-        reset.addMouseListener(new MouseAdapter()
+        resetButton.addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent evt)
             {
                 buttonPressed(evt);
             }
         });
-        countdown = new Thread(){
+        //thread for the stopwatch.
+        new Thread(){
             public void run(){
                 long time = 0;
                 while(!Thread.currentThread().isInterrupted()) {
@@ -58,22 +66,43 @@ public class App{
                         while(time < 90000){
                             time = System.currentTimeMillis() - startTime;
                             //System.out.println(time);
-                            display.setText(Long.toString(time));
+                            lapModel.setElementAt(Long.toString(time), 0);
+
+                            if(reset){
+                                if(lapCounter < 6){
+
+                                    lapCounter++;
+                                    reset = false;
+                                }
+                                else{
+                                    lapCounter = 1;
+
+                                    lapCounter++;
+                                    reset = false;
+
+                                }
+                                pause = true;
+                                break;
+                            }
+                            if(pause){
+
+                            }
                         }
                     }
                 }
             }
-        };
-    countdown.start();
+        }.start();
+
 }
 
     private static void buttonPressed(MouseEvent e){
         if(e.getSource() == startStop){
+            reset = false;
             pause = false;
             System.out.println(pause);
         }
-        if(e.getSource() == reset){
-            pause = true;
+        if(e.getSource() == resetButton){
+            reset = true;
             System.out.println(pause);
 
         }
